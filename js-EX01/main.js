6139d15ef33612c3f1cac84ced27efdd004e0538
David Leake <dleake14@gmail.com>
2019-03-16 14:50:27 -0500

    Adding NBA-GO

--- js-EX01/main.js
+++ js-EX01/main.js
@@ -2,7 +2,16 @@
 
 
 function output () {
-    let out = "Let's get this text into the red";
-    let output = document.writeln(out);
-
+    var line1;
+    var line2;
+    line1 = document.querySelector('line1');
+    line2 = document.querySelector('line2');
+    let home = "Oklahoma";
+    let away = "Texas";
+    let hScore = 14;
+    let aScore = 0;
+    let qtr = 1;
+    line1.textContent = [away] + "QTR" + [home];
+    line2.textContent = [aScore] + [qtr] + [hScore];
 }
+
