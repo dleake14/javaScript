430a8088c536032cc0573a941b1946328c5cf3c4
David Leake <dleake14@gmail.com>
2019-03-17 17:44:37 -0500

    Got it started

---
+++ js-EX01/bin/main.js
@@ -0,0 +1,23 @@
+    output();
+
+
+
+
+function output () {
+    var line1;
+    var line2;
+    line1 = document.querySelector('line1');
+    line2 = document.querySelector('line2');
+    let home = "Oklahoma";
+    let away = "Texas";
+    let hScore = 14;
+    let aScore = 0;
+    let qtr = 1;
+    let pad = 0;
+    let awayPad = parseInt((12 / 2) - (away.length / 2));
+    line1 = [away].toString().toUpperCase().padStart(10,'F');
+
+   /* line1.textContent = [away].toString().toUpperCase().padStart(3,'#').padEnd(3,'#') + "QTR" + [home];*/
+    /*line2.textContent = [aScore] + [qtr] + [hScore] ;*/
+}
+
--- js-EX01/main.js
+++ js-EX01/main.js
@@ -1,23 +1,45 @@
-    output();
+function padder(str, lineNum) {
+    var pstart = 0, pend = 0;
+    var odder = 0;
+    if ((str.length % 2) == 1){
+        odder = 1;
+    }
+    pstart =  (str.length) + ((lineLength / 2).toFixed(0) - (((str.length) - odder) / 2).toFixed(0));
+    pend =  lineLength;
+    var out = str.padStart([pstart]).padEnd([pend]);
 
-
-
-
-function output () {
-    var line1;
-    var line2;
-    line1 = document.querySelector('line1');
-    line2 = document.querySelector('line2');
-    let home = "Oklahoma";
-    let away = "Texas";
-    let hScore = 14;
-    let aScore = 0;
-    let qtr = 1;
-    let pad = 0;
-    let awayPad = parseInt((12 / 2) - (away.length / 2));
-    line1 = [away].toString().toUpperCase().padStart(10,'F');
-    
-   /* line1.textContent = [away].toString().toUpperCase().padStart(3,'#').padEnd(3,'#') + "QTR" + [home];*/
-    /*line2.textContent = [aScore] + [qtr] + [hScore] ;*/
+    if (lineNum ==1) {
+        appendOne(out);
+    }
+    else {appendTwo(out)}
 }
+
+
+var lineLength = 12;
+var line1 = document.getElementById('line1');
+var line2 = document.getElementById('line2');
+let home = "Oklahoma";
+let qtrTxt = "QTR"
+let away = "Texas";
+let hScore = 14;
+let aScore = 0;
+let qtr = 1;
+let pad = 0;
+
+padder(away, 1);
+padder(qtrTxt, 1);
+padder(home, 1);
+padder(aScore.toString(), 2);
+padder(qtr.toString(), 2);
+padder(hScore.toString(), 2);
+
+
+function appendOne(out){
+    line1.textContent += out;
+}
+
+function appendTwo(out){
+    line2.textContent += out;
+}
+
 
